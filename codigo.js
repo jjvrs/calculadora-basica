df0f3e483daf7f9c16cf9d5812cdd478f5358d80
jose vasquez <jjvrs007@gmail.com>
2021-11-05 09:17:26 -0400

    se agrego la funcionalidad a los botones restar,multiplicar y dividir

--- codigo.js
+++ codigo.js
@@ -10,16 +10,37 @@ const sumar = ()=>{
 
 const n1 = parseInt( document.querySelector('#n1').value);
 const n2 = parseInt( document.querySelector('#n2').value);
-if ( n1 < 0 && n2 < 0 ) {
- console.log(`estos numeros ${n1} y ${n2}no son permitidos`);   
-}else{
 
-    const res = n1+n2;
+    const res = (n1 + n2);
 console.log("el resultado de la suma es =",res);
 }
 
 
 
+const restar = ()=>{
 
-}
+    const n1 = parseInt( document.querySelector('#n1').value);
+    const n2 = parseInt( document.querySelector('#n2').value);
+    
+        const res = (n1 - n2);
+    console.log("el resultado de la suma es =",res);
+    }
 
+
+const multi = ()=>{
+
+    const n1 = parseInt( document.querySelector('#n1').value);
+    const n2 = parseInt( document.querySelector('#n2').value);
+        
+        const res = (n1 * n2);
+    console.log("el resultado de la suma es =",res);
+    }
+
+const dividir = ()=>{
+
+    const n1 = parseInt( document.querySelector('#n1').value);
+    const n2 = parseInt( document.querySelector('#n2').value);
+            
+        const res = (n1 / n2);
+    console.log("el resultado de la suma es =",res);
+    }
